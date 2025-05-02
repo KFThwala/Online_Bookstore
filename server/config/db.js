610d88c2f68ff900a8fc4f5dc100beaa7cf47bd1
KFThwala <kagithwala@gmail.com>
2025-05-02 19:16:50 +0200

    fixing mongodb connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,13 @@
 import mongoose from "mongoose";
+import { config } from "dotenv";
+
+config()
 
 const connect = async() => {
     try {
         await mongoose.connect(process.env.MONGODB_URI);
         console.log("MongoDB connected successfully!");
+        
     } catch (error) {
         console.error("Failed to connect to MongoDB:", error.message);
         process.exit(1);
